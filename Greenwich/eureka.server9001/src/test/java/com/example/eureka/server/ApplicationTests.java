190b7f8870f02c0de439193192e7077594f403e5
zhaokejin <210006540@qq.com>
2019-09-07 20:43:37 +0800

    Greenwich版本集群eureka

---
+++ Greenwich/eureka.server9001/src/test/java/com/example/eureka/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka.server;
+
+import org.junit.Test;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
